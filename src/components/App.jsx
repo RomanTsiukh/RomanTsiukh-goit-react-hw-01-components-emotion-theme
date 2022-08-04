0f3c217e422c78a75c4a4e12bd845eb0405a22da
Roman <95884644+RomanTsiukh@users.noreply.github.com>
2022-08-05 01:33:41 +0300

    a

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,13 @@
-import { Profile } from './Profile/Profile'
-import { StatisticsList } from './Statistics/StatisticsList';
-import { FriendList } from './Friends/FriendList';
-import { TransactionHistory } from './Transaction/TransactionHistory';
-import user from './Profile/user.json';
-import stats from './Statistics/data.json';
-import friends from './Friends/friends.json';
-import transactions from './Transaction/transactions.json';
+import styled from '@emotion/styled';
+
+const Text = styled.p`
+  color: ${props => props.theme.colors.text};
+`;
 
 export function App() {
-    return(
-      <div>
-        <Profile 
-          link={user.avatar} 
-          usname={user.username}
-          ustag={user.tag} 
-          locat={user.location} 
-          folowers={user.stats.followers} 
-          views={user.stats.views} 
-          likes={user.stats.likes}
-        />
-        
-        <StatisticsList title="Upload stats" stats={stats}/>
-        <FriendList friends={friends}/>
-        <TransactionHistory items={transactions} />
-      </div>
-    )};
-
+  return (
+    <Text>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, neque.
+    </Text>
+  );
+}
